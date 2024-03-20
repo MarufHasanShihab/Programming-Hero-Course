7ebe74cef6be1e08e9f25693860b0fc573de74bc
Maruf Hasan Shihab <marufhasanshihab2@gmail.com>
2024-03-20 16:17:23 +0600

    added new project

---
+++ Javascript/Milestone-5/Conceptual Sessions/Dom Manipulation (Gias)/FLower-shop-Dom/index.js
@@ -0,0 +1,12 @@
+// finding element
+const selectedTtems = document.getElementById("selected-items");
+let toTal = document.getElementById("total");
+// handle click btn
+function handleCLikBtn(target) {
+  const iteamTitle = target.parentNode.childNodes[1].innerText;
+  const li = document.createElement("li");
+  li.innerText = iteamTitle;
+  selectedTtems.appendChild(li);
+  const price = target.parentNode.childNodes[5].innerText.split(" ")[1];
+  toTal.innerText = parseFloat(toTal.innerText) + parseFloat(price);
+}
